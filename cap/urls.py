86a24d9b943ad4483e9e553ae3f5f5787244f724
maf68 <113141328+maf68@users.noreply.github.com>
2024-03-31 14:49:21 +0300

    added comments

--- cap/urls.py
+++ cap/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('report/', views.report_view, name='report_form'),
     path('ajax/load_sector_names/', views.load_sector_names, name='ajax_load_sector_names'),
+    # we wont use the load sector path, its just for ajax purposes.
 ]
