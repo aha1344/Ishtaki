cd0be5ca132f6b6a523fdf4f52f8e5841675404c
maf68 <maf68@mail.aub.edu>
2024-05-16 13:47:39 +0300

    fixedDB settings

--- report/models.py
+++ report/models.py
@@ -3,8 +3,8 @@ from .choices import corruption_types, sector_types, cities
 # Create your models here.
 class Report(models.Model):
     corruption_type = models.CharField(max_length = 100, choices = corruption_types)
-    public_sector_type = models.CharField(max_length = 100, choices = sector_types, default = "")
-    public_sector_name = models.CharField(max_length = 100, default = "")
+    public_sector_type = models.CharField(max_length = 500, choices = sector_types, default = "")
+    public_sector_name = models.CharField(max_length = 500, default = "")
     date_of_incident = models.DateTimeField()
     city = models.CharField(max_length=100, choices = cities, default = "بيروت")
     street = models.CharField(max_length=100, default = "")
