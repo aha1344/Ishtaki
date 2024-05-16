33b0565d1fa9785fdd408b335f80de42ff490b42
Ali Faour <aaf36@mail.aub.edu>
2024-05-16 14:06:17 +0300

    added layers to each corruption type and corresponding legend

--- report/urls.py
+++ report/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
 from . import views
+from .views import corruption_heatmap_view, corruption_heatmap_data_view, get_choices_view
 
 urlpatterns = [
     path('report/', views.report_view, name='report_form'),
     path('ajax/load_sector_names/', views.load_sector_names, name='ajax_load_sector_names'),
     # we wont use the load sector path, its just for ajax purposes.
     path('home/', views.HomePage, name="HomePage"),
+    path('heatmap/', corruption_heatmap_view, name='corruption_heatmap'),
+    path('corruption-heatmap/', corruption_heatmap_data_view, name='corruption_heatmap_data'),
+    path('choices/', get_choices_view, name='get_choices'),
 ]
--- report/views.py
+++ report/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
-from .choices import hospitals, ministries, institutions, prisons, banks, courts, universities, military, police, general_security, sector_types
+from .choices import hospitals, ministries, institutions, prisons, banks, courts, universities, sector_types
 from django.http import JsonResponse
 from .forms import ReportForm
 from .models import Report
 from django.db.models import Count
+from django.http import JsonResponse
+from django.views.decorators.http import require_GET
+from .choices import corruption_types, sector_types, cities_coordinates
 
 
 
@@ -25,10 +28,6 @@ def load_sector_names(request):
         names = courts
     elif sector_type == 'universities':
         names = universities
-    elif sector_type == 'military':
-        names = military
-    elif sector_type == 'police':
-        names = police
     elif sector_type == 'general_security':
         names = general_security
     return JsonResponse(names, safe=False)
@@ -68,3 +67,41 @@ def statistics_view(request):
 def HomePage(request):
     sectors  = [sector[1] for sector in sector_types]
     return render(request, "report/home.html", {"sectors":sectors})
+
+
+@require_GET
+def corruption_heatmap_data_view(request):
+    corruption_type = request.GET.get('corruption_type')
+    public_sector_type = request.GET.get('public_sector_type')
+
+    reports = Report.objects.all()
+    if corruption_type:
+        reports = reports.filter(corruption_type=corruption_type)
+    if public_sector_type:
+        reports = reports.filter(public_sector_type=public_sector_type)
+
+    data = []
+    for report in reports:
+        city = report.city
+        if city in cities_coordinates:
+            latitude, longitude = cities_coordinates[city]
+            data.append({
+                'latitude': latitude,
+                'longitude': longitude,
+                'corruption_type': report.corruption_type,
+                'public_sector_type': report.public_sector_type,
+                'public_sector_name': report.public_sector_name,
+                'date_of_incident': report.date_of_incident.isoformat(),
+                'street': report.street,
+            })
+
+    return JsonResponse(data, safe=False)
+
+def corruption_heatmap_view(request):
+    return render(request, 'report/heatmap.html')
+
+def get_choices_view(request):
+    return JsonResponse({
+        'corruption_types': corruption_types,
+        'sector_types': sector_types,
+    })
